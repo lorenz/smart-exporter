df0e62843216bc7f3ff6d6e39311a093ee14d625
Lorenz Brun <lorenz@brun.one>
2020-07-06 21:21:31 +0200

    Use a collector for SMART values (fixes stale metrics)

--- main.go
+++ main.go
@@ -19,8 +19,6 @@ import (
 )
 
 var (
-	smartValue             = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "ata_smart_value", Help: "ATA SMART normalized value"}, []string{"dev", "serial", "model", "family", "attr_id", "attr_name"})
-	smartRawValue          = prometheus.NewGaugeVec(prometheus.GaugeOpts{Name: "ata_smart_raw_value", Help: "ATA SMART raw decoded value"}, []string{"dev", "serial", "model", "family", "attr_id", "attr_name"})
 	collectorDurationValue = prometheus.NewHistogram(prometheus.HistogramOpts{Name: "ata_smart_collector_duration_seconds", Buckets: prometheus.ExponentialBuckets(0.01, 2.0, 10)})
 )
 
@@ -32,71 +30,99 @@ var (
 	listenAddr = flag.String("listen-addr", ":9541", "Address the SMART exporter should listen on")
 )
 
-func main() {
-	prometheus.MustRegister(smartValue)
-	prometheus.MustRegister(smartRawValue)
-	prometheus.MustRegister(collectorDurationValue)
-	flag.Parse()
-	http.Handle("/metrics", promhttp.Handler())
-	go http.ListenAndServe(*listenAddr, nil)
+type smartCollector struct {
+	smartValue    *prometheus.Desc
+	smartRawValue *prometheus.Desc
+	db            drivedb.DriveDb
+}
+
+func newSMARTCollector() *smartCollector {
 	db, err := drivedb.OpenDriveDb("drivedb.yaml")
 	if err != nil {
 		panic(err)
 	}
-	for {
-		devs, err := ioutil.ReadDir("/dev")
-		if err != nil {
-			log.Printf("Failed to list devices: %v", err)
-			continue
+	return &smartCollector{
+		smartValue:    prometheus.NewDesc("ata_smart_value", "ATA SMART normalized value", []string{"dev", "serial", "model", "family", "attr_id", "attr_name"}, nil),
+		smartRawValue: prometheus.NewDesc("ata_smart_raw_value", "ATA SMART raw decoded value", []string{"dev", "serial", "model", "family", "attr_id", "attr_name"}, nil),
+		db:            db,
+	}
+}
+
+func (c *smartCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.smartValue
+	ch <- c.smartRawValue
+}
+
+func (c *smartCollector) Collect(ch chan<- prometheus.Metric) {
+	devs, err := ioutil.ReadDir("/dev")
+	if err != nil {
+		log.Printf("Failed to list devices: %v", err)
+		return
+	}
+	var diskDevs []string
+	for _, dev := range devs {
+		if sdRegex.MatchString(dev.Name()) {
+			diskDevs = append(diskDevs, dev.Name())
 		}
-		var diskDevs []string
-		for _, dev := range devs {
-			if sdRegex.MatchString(dev.Name()) {
-				diskDevs = append(diskDevs, dev.Name())
+	}
+	wg := sync.WaitGroup{}
+	for _, disk := range diskDevs {
+		wg.Add(1)
+		go func(disk string) {
+			startTime := time.Now()
+			defer func() {
+				duration := time.Now().Sub(startTime)
+				collectorDurationValue.Observe(duration.Seconds())
+			}()
+			defer wg.Done()
+			diskHandle, err := os.Open("/dev/" + disk)
+			if err != nil {
+				log.Printf("Failed to open device %v: %v", disk, err)
+				return
 			}
-		}
-		wg := sync.WaitGroup{}
-		for _, disk := range diskDevs {
-			wg.Add(1)
-			go func(disk string) {
-				startTime := time.Now()
-				defer func() {
-					duration := time.Now().Sub(startTime)
-					collectorDurationValue.Observe(duration.Seconds())
-				}()
-				defer wg.Done()
-				diskHandle, err := os.Open("/dev/" + disk)
+			defer diskHandle.Close()
+			id, err := smart.Identify(diskHandle)
+			if err != nil {
+				return
+			}
+			if id.Word85&1 == 1 { // Disk has SMART
+				pages, err := smart.SmartReadData(diskHandle)
 				if err != nil {
-					log.Printf("Failed to open device %v: %v", disk, err)
+					log.Printf("Failed to get SMART data: %v", err)
 					return
 				}
-				defer diskHandle.Close()
-				id, err := smart.Identify(diskHandle)
-				if err != nil {
-					return
-				}
-				if id.Word85&1 == 1 {
-					pages, err := smart.SmartReadData(diskHandle)
-					if err != nil {
-						log.Printf("Failed to get SMART data: %v", err)
-						return
+				model := c.db.LookupDrive(id.ModelNumber())
+				for _, attr := range pages.Attrs {
+					attrIdStr := strconv.Itoa(int(attr.Id))
+					if attr.Id == 0 {
+						continue
 					}
-					model := db.LookupDrive(id.ModelNumber())
-					for _, attr := range pages.Attrs {
-						attrIdStr := strconv.Itoa(int(attr.Id))
-						conv, ok := model.Presets[attrIdStr]
-						if ok {
-							metricValue := attr.RawValue(conv.Conv)
-							if metricValue >= 0.0 {
-								smartRawValue.WithLabelValues(disk, strings.TrimSpace(string(id.SerialNumber())), strings.TrimSpace(string(id.ModelNumber())), model.Family, attrIdStr, conv.Name).Set(metricValue)
-							}
+					conv, ok := model.Presets[attrIdStr]
+					labels := []string{disk, strings.TrimSpace(string(id.SerialNumber())), strings.TrimSpace(string(id.ModelNumber())), model.Family, attrIdStr, conv.Name}
+					if ok {
+						metricValue := attr.RawValue(conv.Conv)
+						if metricValue >= 0.0 {
+							ch <- prometheus.MustNewConstMetric(c.smartRawValue, prometheus.UntypedValue, metricValue, labels...)
 						}
-						smartValue.WithLabelValues(disk, strings.TrimSpace(string(id.SerialNumber())), strings.TrimSpace(string(id.ModelNumber())), model.Family, attrIdStr, conv.Name).Set(float64(attr.Value))
 					}
+					ch <- prometheus.MustNewConstMetric(c.smartValue, prometheus.UntypedValue, float64(attr.Value), labels...)
 				}
-			}(disk)
-		}
-		wg.Wait()
+			}
+		}(disk)
+	}
+	wg.Wait()
+}
+
+func main() {
+	smartC := newSMARTCollector()
+	prometheus.MustRegister(smartC)
+	prometheus.MustRegister(collectorDurationValue)
+	flag.Parse()
+	http.Handle("/metrics", promhttp.Handler())
+	go http.ListenAndServe(*listenAddr, nil)
+
+	for {
+
 		time.Sleep(1 * time.Minute)
 	}
 }
